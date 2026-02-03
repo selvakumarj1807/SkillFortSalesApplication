c35f51f0e783f1b60a4d7c44b78a093db51d3cd5
selvakumarj1807 <selvakumarj1807@gmail.com>
2026-02-03 11:46:21 +0530

    update notification

--- frontend/src/layout/AdminAppHeader.tsx
+++ frontend/src/layout/AdminAppHeader.tsx
@@ -3,7 +3,6 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useSidebar } from "../context/SidebarContext";
 import { ThemeToggleButton } from "../components/common/ThemeToggleButton";
-import NotificationDropdown from "../components/header/NotificationDropdown";
 import AdminUserDropdown from "../components/header/AdminUserDropdown";
 
 const AppHeader: React.FC = () => {
--- frontend/src/layout/AppHeader.tsx
+++ frontend/src/layout/AppHeader.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useSidebar } from "../context/SidebarContext";
 import { ThemeToggleButton } from "../components/common/ThemeToggleButton";
-import NotificationDropdown from "../components/header/NotificationDropdown";
+//import NotificationDropdown from "../components/header/NotificationDropdown";
 import UserDropdown from "../components/header/UserDropdown";
 
 const AppHeader: React.FC = () => {
@@ -127,7 +127,7 @@ const AppHeader: React.FC = () => {
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
-            <NotificationDropdown />
+           {/* <NotificationDropdown /> */}
             {/* <!-- Notification Menu Area --> */}
           </div>
           {/* <!-- User Area --> */}
